210202a223720848e712186d85c99e763651772e
Moncef Ben-Soula <mbensoula@intouchtechnology.com>
2014-07-07 21:03:14 -0700

    removed unused redirect variable

--- src/main/java/threescale/v3/api/impl/ServiceApiDriver.java
+++ src/main/java/threescale/v3/api/impl/ServiceApiDriver.java
@@ -1,149 +1,157 @@
 package threescale.v3.api.impl;
 
-import threescale.v3.api.*;
+import threescale.v3.api.AuthorizeResponse;
+import threescale.v3.api.HttpResponse;
+import threescale.v3.api.ParameterMap;
+import threescale.v3.api.ReportResponse;
+import threescale.v3.api.ServerAccessor;
+import threescale.v3.api.ServerError;
+import threescale.v3.api.ServiceApi;
 
 /**
  * Concrete implementation of the ServiceApi.
- *
+ * 
  * @see ServiceApi
  */
 public class ServiceApiDriver implements ServiceApi {
 
-    private String provider_key = null;
-    private String host = DEFAULT_HOST;
-    private boolean useHttps = false;
-    private String redirect_url = "http://localhost:8080/oauth/oauth_redirect";
+	private String provider_key = null;
+	private String host = DEFAULT_HOST;
+	private boolean useHttps = false;
 
-    private ServerAccessor server = null;
+	private ServerAccessor server = null;
 
-    public ServiceApiDriver() {
-        this.server = new ServerAccessorDriver();
+	public ServiceApiDriver() {
+		this.server = new ServerAccessorDriver();
 
-    }
+	}
 
-    public ServiceApiDriver(String provider_key) {
-        this.provider_key = provider_key;
-        this.server = new ServerAccessorDriver();
-    }
-    
-    public ServiceApiDriver(String provider_key, boolean useHttps) {
-        this.provider_key = provider_key;
-        this.useHttps = useHttps;
-        this.server = new ServerAccessorDriver();
-    }
+	public ServiceApiDriver(String provider_key) {
+		this.provider_key = provider_key;
+		this.server = new ServerAccessorDriver();
+	}
 
-    public ServiceApiDriver(String provider_key, String host) {
-        this.provider_key = provider_key;
-        this.host = host;
-        this.server = new ServerAccessorDriver();
-    }
-    
-    public ServiceApiDriver(String provider_key, String host, boolean useHttps) {
-        this.provider_key = provider_key;
-        this.host = host;
-        this.useHttps = useHttps;
-        this.server = new ServerAccessorDriver();
-    }
+	public ServiceApiDriver(String provider_key, boolean useHttps) {
+		this.provider_key = provider_key;
+		this.useHttps = useHttps;
+		this.server = new ServerAccessorDriver();
+	}
 
-    public AuthorizeResponse authrep(ParameterMap metrics) throws ServerError {
-        metrics.add("provider_key", provider_key);
+	public ServiceApiDriver(String provider_key, String host) {
+		this.provider_key = provider_key;
+		this.host = host;
+		this.server = new ServerAccessorDriver();
+	}
 
-        ParameterMap usage = metrics.getMapValue("usage");
+	public ServiceApiDriver(String provider_key, String host, boolean useHttps) {
+		this.provider_key = provider_key;
+		this.host = host;
+		this.useHttps = useHttps;
+		this.server = new ServerAccessorDriver();
+	}
 
-        if (usage == null || usage.getStringValue("hits") == null) {
-            if (usage == null) {
-                usage = new ParameterMap();
-                metrics.add("usage", usage);
-            }
-            usage.add("hits", "1");
-        }
-        String urlParams = encodeAsString(metrics);
+	@Override
+	public AuthorizeResponse authrep(ParameterMap metrics) throws ServerError {
+		metrics.add("provider_key", provider_key);
 
-        final String s = getFullHostUrl() + "/transactions/authrep.xml?" + urlParams;
-//        System.out.println("Actual: " + s);
+		ParameterMap usage = metrics.getMapValue("usage");
 
-        HttpResponse response = server.get(s);
-        if (response.getStatus() == 500) {
-            throw new ServerError(response.getBody());
-        }
-        return convertXmlToAuthorizeResponse(response);
-    }
+		if (usage == null || usage.getStringValue("hits") == null) {
+			if (usage == null) {
+				usage = new ParameterMap();
+				metrics.add("usage", usage);
+			}
+			usage.add("hits", "1");
+		}
+		String urlParams = encodeAsString(metrics);
 
-    public ReportResponse report(String service_id, ParameterMap... transactions) throws ServerError {
-        if (transactions == null || transactions.length == 0)
-            throw new IllegalArgumentException("No transactions provided");
+		final String s = getFullHostUrl() + "/transactions/authrep.xml?" + urlParams;
+		// System.out.println("Actual: " + s);
 
-        ParameterMap params = new ParameterMap();
-        params.add("provider_key", provider_key);
-        if (service_id != null) {
-            params.add("service_id", service_id);
-        }
-        ParameterMap trans = new ParameterMap();
-        params.add("transactions", transactions);
+		HttpResponse response = server.get(s);
+		if (response.getStatus() == 500) {
+			throw new ServerError(response.getBody());
+		}
+		return convertXmlToAuthorizeResponse(response);
+	}
 
-        int index = 0;
-        for (ParameterMap transaction : transactions) {
-            trans.add("" + index, transaction);
-            index++;
-        }
+	@Override
+	public ReportResponse report(String service_id, ParameterMap... transactions) throws ServerError {
+		if (transactions == null || transactions.length == 0) {
+			throw new IllegalArgumentException("No transactions provided");
+		}
 
-        HttpResponse response = server.post(getFullHostUrl() + "/transactions.xml", encodeAsString(params));
-        if (response.getStatus() == 500) {
-            throw new ServerError(response.getBody());
-        }
-        return new ReportResponse(response);
-    }
+		ParameterMap params = new ParameterMap();
+		params.add("provider_key", provider_key);
+		if (service_id != null) {
+			params.add("service_id", service_id);
+		}
+		ParameterMap trans = new ParameterMap();
+		params.add("transactions", transactions);
 
-    public AuthorizeResponse authorize(ParameterMap parameters) throws ServerError {
-        parameters.add("provider_key", provider_key);
-        String urlParams = encodeAsString(parameters);
+		int index = 0;
+		for (ParameterMap transaction : transactions) {
+			trans.add("" + index, transaction);
+			index++;
+		}
 
-        final String s = getFullHostUrl() + "/transactions/authorize.xml?" + urlParams;
-        HttpResponse response = server.get(s);
-        if (response.getStatus() == 500) {
-            throw new ServerError(response.getBody());
-        }
-        return convertXmlToAuthorizeResponse(response);
-    }
+		HttpResponse response = server.post(getFullHostUrl() + "/transactions.xml", encodeAsString(params));
+		if (response.getStatus() == 500) {
+			throw new ServerError(response.getBody());
+		}
+		return new ReportResponse(response);
+	}
 
-    public String getHost() {
-        return host;
-    }
+	@Override
+	public AuthorizeResponse authorize(ParameterMap parameters) throws ServerError {
+		parameters.add("provider_key", provider_key);
+		String urlParams = encodeAsString(parameters);
 
-    public AuthorizeResponse oauth_authorize(ParameterMap params) throws ServerError {
-        params.add("provider_key", provider_key);
+		final String s = getFullHostUrl() + "/transactions/authorize.xml?" + urlParams;
+		HttpResponse response = server.get(s);
+		if (response.getStatus() == 500) {
+			throw new ServerError(response.getBody());
+		}
+		return convertXmlToAuthorizeResponse(response);
+	}
 
-        String urlParams = encodeAsString(params);
+	@Override
+	public String getHost() {
+		return host;
+	}
 
-        final String s = getFullHostUrl() + "/transactions/oauth_authorize.xml?" + urlParams;
-//        System.out.println("Actual: " + s);
+	@Override
+	public AuthorizeResponse oauth_authorize(ParameterMap params) throws ServerError {
+		params.add("provider_key", provider_key);
 
-        HttpResponse response = server.get(s);
-        if (response.getStatus() == 500) {
-            throw new ServerError(response.getBody());
-        }
-        return convertXmlToAuthorizeResponse(response);
-    }
+		String urlParams = encodeAsString(params);
 
-    private String getFullHostUrl() {
-        
-        return useHttps ? "https://" + getHost() : "http://" + getHost();
-    }
+		final String s = getFullHostUrl() + "/transactions/oauth_authorize.xml?" + urlParams;
+		// System.out.println("Actual: " + s);
 
+		HttpResponse response = server.get(s);
+		if (response.getStatus() == 500) {
+			throw new ServerError(response.getBody());
+		}
+		return convertXmlToAuthorizeResponse(response);
+	}
 
-    public String encodeAsString(ParameterMap params) {
-        ParameterEncoder encoder = new ParameterEncoder();
-        return encoder.encode(params);
-    }
+	private String getFullHostUrl() {
 
+		return useHttps ? "https://" + getHost() : "http://" + getHost();
+	}
 
-    private AuthorizeResponse convertXmlToAuthorizeResponse(HttpResponse res) throws ServerError {
-        return new AuthorizeResponse(res.getStatus(), res.getBody());
-    }
+	public String encodeAsString(ParameterMap params) {
+		ParameterEncoder encoder = new ParameterEncoder();
+		return encoder.encode(params);
+	}
 
+	private AuthorizeResponse convertXmlToAuthorizeResponse(HttpResponse res) throws ServerError {
+		return new AuthorizeResponse(res.getStatus(), res.getBody());
+	}
 
-    public ServiceApiDriver setServer(ServerAccessor server) {
-        this.server = server;
-        return this;
-    }
+	public ServiceApiDriver setServer(ServerAccessor server) {
+		this.server = server;
+		return this;
+	}
 }
