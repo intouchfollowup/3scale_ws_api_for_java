3d10c6ba27a6ee4e39827e9d0bee4d28bcf2b433
Moncef Ben-Soula <mbensoula@intouchtechnology.com>
2014-07-09 07:35:29 -0700

    added small javadoc

--- src/main/java/threescale/v3/api/AccountApi.java
+++ src/main/java/threescale/v3/api/AccountApi.java
@@ -2,7 +2,25 @@ package threescale.v3.api;
 
 import threescale.v3.api.http.response.AccountResponse;
 
+/**
+ *
+ * Account Management API interface.
+ *
+ * @author Moncef
+ *
+ */
 public interface AccountApi {
 
+	/**
+	 * This request allows to reproduce a sign-up from a buyer in a single API call. It will create
+	 * an Account, an admin User for the account and one Application with its keys. If the plan_id
+	 * are not passed the default plans will be used instead. You can add additional custom
+	 * parameters that you define in Fields Definition on your Admin Portal.
+	 *
+	 * @param parameters - {@link ParameterMap}
+	 * @return {@link AccountResponse}
+	 *
+	 * @throws ServerError
+	 */
     public AccountResponse signup(ParameterMap parameters) throws ServerError;
 }
