eadcd6b8dece0f521a083e03f9308e77280a0480
Moncef Ben-Soula <mbensoula@intouchtechnology.com>
2014-07-09 07:40:31 -0700

    added constructors to force providing from host, which is required for the Account Management API

--- src/main/java/threescale/v3/api/impl/AccountApiDriver.java
+++ src/main/java/threescale/v3/api/impl/AccountApiDriver.java
@@ -7,11 +7,21 @@ import threescale.v3.api.ServerError;
 import threescale.v3.api.http.response.AccountResponse;
 
 /**
+ * {@link AccountApi} implementation that requires the providing of a host, i.e. the
+ * Account Management API does not have default host like the Service Management API.
  *
  * @author Moncef
  *
  */
-public class AcountApiDriver extends ApiDriver implements AccountApi{
+public class AccountApiDriver extends ApiDriver implements AccountApi{
+
+	public AccountApiDriver(String providerKey, String host, boolean useHttps) {
+		super(providerKey, host, useHttps);
+	}
+
+	public AccountApiDriver(String providerKey, String host) {
+		super(providerKey, host);
+	}
 
 	@Override
 	public AccountResponse signup(ParameterMap parameters) throws ServerError {
